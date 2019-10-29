eadb96fb7d58ae72a78681bf138be1953688ba3f
WeydsonBroeto <weydson.broeto@estudante.uffs.edu.br>
2019-10-29 20:30:51 -0300

    certo

--- divisao.py
+++ divisao.py
@@ -1,3 +1,8 @@
 a=int(input("digite o primeiro numero"))
 b=int(input("digite o segundo numero"))
-print(a/b)
+resultado=a/b
+print (resultado)
+cont=1
+while  cont<=resultado:
+	print ("o curso e", cont*"muito ", "bom")
+	cont +=1
